67092c32857bcca914023f9e7f4be045ef126ef3
OB1ADAET <116683120+OB1ADAET@users.noreply.github.com>
2023-03-16 16:04:35 +0300

    Add files via upload

---
+++ mysql1/mysql1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using mysql1.mysql1DataSetTableAdapters;
+
+namespace mysql1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        usersTableAdapter usersTableAdapter = new usersTableAdapter ();
+        public MainWindow()
+        {
+            InitializeComponent();
+            datatop.ItemsSource = usersTableAdapter.GetData();
+            sas1.ItemsSource = usersTableAdapter.GetData ();
+            sas1.DisplayMemberPath = "fname";
+        }
+
+        private void sas1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show((sas1.SelectedItem as DataRowView).Row[2].ToString());
+        }
+    }
+}
